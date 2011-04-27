afe8550fb2c918ffdf36e7e99dc783231f541d6d
ivan <ivan@90f47898-201c-42e4-a952-0c166423ae87>
2011-04-27 07:15:18 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@192 90f47898-201c-42e4-a952-0c166423ae87

--- app/controllers/media_controller.php
+++ app/controllers/media_controller.php
@@ -2185,7 +2185,7 @@ echo'</pre>';
 					foreach ($lst as $key => $val)
 					{
 						//$lst[$key]['Vbpost']['pagetext'] = $uuConverter->unicodeToUtf(Utils::transUbbTags($val['Vbpost']['pagetext']));
-						$lst[$key]['Vbpost']['pagetext'] = Utils::transUbbTags($val['Vbpost']['pagetext']);
+						$lst[$key]['Vbpost']['pagetext'] = iconv('windows-1252', 'utf-8', Utils::transUbbTags($val['Vbpost']['pagetext']));
 					}
 					$threadInfo['stat'] = __('showing', true) . ' ' . count($lst) . ' ' . __('posts. Total', true) . ' ' . $threadData['Thread']['replycount'];
 				}
