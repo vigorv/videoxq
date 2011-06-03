8a2916503a922c8b4722a49fda4d0322a644951c
ivan <ivan@90f47898-201c-42e4-a952-0c166423ae87>
2011-06-03 02:45:19 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@296 90f47898-201c-42e4-a952-0c166423ae87

--- app/app_controller.php
+++ app/app_controller.php
@@ -114,19 +114,19 @@ $geoInfo['city'] = 'city';
 $this->Session->write('geoInfo', $geoInfo);
 */
 //*/
-		if (empty($geoInfo['Geoip']['region_id']))
-		{
-			$regionLang = _ENG_;
-//$regionLang = _RUS_;
-		}
-		else
-		{
-			$regionLang = _RUS_;
-		}
 		$lang = $this->Session->read("language");
-//$lang = 0;
 		if (empty($lang))
 		{
+			if (empty($geoInfo['Geoip']['region_id']))
+			{
+				$regionLang = _ENG_;
+	//$regionLang = _RUS_;
+			}
+			else
+			{
+				$regionLang = _RUS_;
+			}
+	//$lang = 0;
 			$lang = $regionLang;
 			$this->Session->write("language", $lang);
 		}
