f5d8c4f061eb1ae11a19eb85713be5b732b99685
ivan <ivan@90f47898-201c-42e4-a952-0c166423ae87>
2011-07-26 03:46:28 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@361 90f47898-201c-42e4-a952-0c166423ae87

---
+++ app/webroot/js/mobile/webkit.js
@@ -0,0 +1,51 @@
+
+function nextScreen(url){
+  
+    var link = $(url).attr("href");
+  
+    $('#nextScreen').load(link,
+        function(){
+            $('#top_bars').hide();
+            $('#home').hide();
+            $('#nextScreen').show().scrollLeft();
+            $('#back_button').show();
+        }
+        );
+ 
+  
+}
+
+function backToHome(){
+    $('#top_bars').show();
+    $('#home').show();
+    $('#nextScreen').hide();
+    $('#back_button').hide();
+    $('#nextScreen').clear();  
+}
+
+
+function swipenext(){
+    var cur= $('.MoviePages li[selected]');
+    ni = cur.next();
+    if (ni){
+        cur.removeAttr("selected");
+        cur.hide();
+        //cur.animate({width: 'toggle'});
+        ni.attr("selected","true");
+        ni.show();
+    //ni.animate({width: 'toggle'});
+    }
+}
+
+function swipeprev(){
+    var cur= $('.MoviePages li[selected]');
+    ni = cur.prev();
+    if (ni){
+        cur.removeAttr("selected");
+        //cur.animate({width: 'toggle'});
+        cur.hide();
+        ni.attr("selected","true");
+        ni.show();
+    //ni.animate({width: 'toggle'});
+    }
+}
