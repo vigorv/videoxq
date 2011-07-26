8eb940476c680981427171e433076cf846190634
ivan <ivan@90f47898-201c-42e4-a952-0c166423ae87>
2011-07-26 07:47:55 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@367 90f47898-201c-42e4-a952-0c166423ae87

---
+++ app/controllers/maina_controller.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Description of mobile_controller
+ *
+ * @author snowing
+ */
+class MainaController extends Controller {
+
+    var $name = 'Maina';
+    var $layout = 'newstyle';
+    var $viewPath = 'maina';
+    var $helpers = array('Html', 'javascript');
+    var $components = array();
+    var $uses = array('Direction', 'News');
+
+    function BeforeFilter() {
+        parent::beforeFilter();
+        Configure::write('debug', 1);
+        $this->out = '';
+        $this->outCount = '';
+        $name = $this->passedArgs;
+        ksort($name);
+        foreach ($name as $k => $v) {
+            $this->out.=$k . "_" . $v . "_";
+            if ($k <> 'page')
+                $this->outCount.=$k . "_" . $v . "_";
+        }
+
+        $lang = Configure::read('Config.language');
+        $this->langFix = '';
+        if ($lang == _ENG_)
+            $this->langFix = '_' . _ENG_;
+
+        $this->set('lang', $lang);
+        $this->set('langFix', $this->langFix);
+        $zone = false;
+        $zones = Configure::read('Catalog.allowedIPs');
+        $zone = checkAllowedMasks($zones, $_SERVER['REMOTE_ADDR'], 1);
+        if ($zone)
+            $this->ImgPath = Configure::read('Catalog.imgPath');
+        else
+            $this->ImgPath = Configure::read('Catalog.imgPathInet');
+        
+           
+                View::set('blocks_top', '/maina/btop');
+        View::set('blocks_right','/maina/bright');
+        View::set('blocks_m_top','/maina/bmtop');
+    }
+
+    function BeforeRender() {
+        parent::BeforeRender();
+
+      
+    }
+
+
+
+    function index() {
+        
+    }
+
+}
