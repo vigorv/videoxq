be1a0716c46a44bcac05ee3f8fd32b62629f2390
ivan <ivan@90f47898-201c-42e4-a952-0c166423ae87>
2011-07-26 03:29:48 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@356 90f47898-201c-42e4-a952-0c166423ae87

--- app/models/user_loginza.php
+++ app/models/user_loginza.php
@@ -11,50 +11,81 @@ class UserLoginza extends AppModel {
         if (!empty($data)) {
             $provider = filter_var($data->provider, FILTER_SANITIZE_STRING);
             $uid = filter_var($data->uid, FILTER_SANITIZE_STRING);
-            $user = $this->query("Select * from userloginza
-               Where provider = " . $provider . " and uid= " . $uid . "
-                   INNER JOIN user on userloginza.loginza_user_id = user.userid
+            $user = $this->query('Select * from userloginza
+                        INNER JOIN user on userloginza.loginza_user_id = user.userid
+                        Where provider ="' . $provider . '" and uid= "' . $uid . '" 
                    LIMIT 1
-               ");
+               ');
             return $user;
         }
         return null;
     }
 
     function AssignProviderToUser($id=0, &$data=null) {
-        if (!empty($data) && ($id>0) ){
+        if (!empty($data) && ($id > 0)) {
             $provider = filter_var($data->provider, FILTER_SANITIZE_STRING);
             $uid = filter_var($data->uid, FILTER_SANITIZE_STRING);
-            return    $userProvider = $this->query("INSERT INTO userloginza (loginza_user_id,provider,uid) VALUES (".$id.",". $provider . "," . $uid . ")");
+            return $userProvider = $this->query("INSERT INTO userloginza (loginza_user_id,provider,uid) VALUES (" . $id . "," . $provider . "," . $uid . ")");
         }
-        else return false;
+        else
+            return false;
     }
 
-    function NewUserByProvider(&$data=null) {
-        if (!empty($data)) {
+    function NewUserByProvider(&$data=null, $pwd=null) {
+
+        function rand_string($length) {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            $size = strlen($chars);
+            $str = '';
+            for ($i = 0; $i < $length; $i++) {
+                $str .= $chars[rand(0, $size - 1)];
+            }
+
+            return $str;
+        }
+
+        if (!empty($data) && ($pwd <> null)) {
             $provider = filter_var($data->provider, FILTER_SANITIZE_STRING);
             $uid = filter_var($data->uid, FILTER_SANITIZE_STRING);
             $email = filter_var($data->email, FILTER_SANITIZE_STRING);
-            //$user->
-          //  return    $userProvider = $this->query("INSERT INTO userloginza (loginza_user_id,provider,uid) VALUES (".$id.",". $provider . "," . $uid . ")");
-        }
-        return false;       
-    }
+            $uname = filter_var($data->name->first_name, FILTER_SANITIZE_STRING);
 
-    function CreateNewProvider() {
-        
+            $user_info['User']['email'] = $email;
+            $user_info['User']['username'] = $uname . '_in' .md5($email.$provider);
+            $user_info['User']['password'] = $pwd;
+            $user_info['User']['usergroupid'] = Configure::read('App.defaultUserGroup');
+            
+
+            $user = new User;
+            $user->create();
+            $res = $user->save($user_info);
+
+            
+            if ($res) {
+                $userid =$res['User']['userid'] ;
+                $this->query("Insert into groups_users (group_id,user_id) 
+                        VALUES (". $user_info['User']['usergroupid'].",".$userid.')');
+                $userProvider = $this->query("INSERT INTO userloginza (loginza_user_id,provider,uid)
+                    VALUES (" .$userid . ',"' . $provider . '","' . $uid . '")');
+
+                return $res;
+            }
+            return null;
+        }
+        return null;
     }
 
     function LoginzaCheck(&$data=null) {
-        if (!exist($data))
+        if (!isset($data))
             return false;
-        if (!exist($data->uid))
+        if (!isset($data->uid))
             return false;
-        if (!exist($data->email))
+        if (!isset($data->email))
             return false;
-        if (!exist($data->provider))
+        if (!isset($data->provider))
             return false;
-        if (exist($data->error_type))
+        if (isset($data->error_type))
             return false;
         return true;
     }
--- app/models/userlottery.php
+++ app/models/userlottery.php
@@ -3,6 +3,19 @@ class Userlottery extends AppModel {
 
 	public $name = 'Userlottery';
 
+	public function getWinners($lotteryId)
+	{
+		$lst = Cache::read('Office.winners', 'office');
+		if (!$lst)
+		{
+    	    $sql = 'SELECT userlotteries.id, userlotteries.winner, userlotteries.user_id as uid, user.username, user.email FROM userlotteries LEFT JOIN user ON (user.userid = userlotteries.user_id AND user.usergroupid <> 3) WHERE userlotteries.winner > 0 AND userlotteries.lottery_id=' . $lotteryId . ' GROUP BY userlotteries.user_id';
+	        $lst = $this->query($sql);
+			Cache::write('Office.winners', $lst, 'office');
+		}
+		return $lst;
+//pr($lst);
+	}
+
     public function getInvitesCnt($lotteryId = 0, $userId = 0)
     {
     	$res = 0;
@@ -16,9 +29,9 @@ class Userlottery extends AppModel {
 	    	    $sql = 'SELECT userlotteries.bid_user_id as uid, COUNT(userlotteries.id) as cnt, user.username FROM userlotteries LEFT JOIN user ON (user.userid = userlotteries.bid_user_id) WHERE userlotteries.bid_user_id > 0 AND userlotteries.lottery_id=' . $lotteryId . ' GROUP BY userlotteries.bid_user_id ORDER BY cnt DESC';
 		        $cnt = $this->query($sql);
 				Cache::write('Office.invitescnt', $cnt, 'office');
-			}
 //echo $sql;
 //pr($cnt);
+			}
 	        if (!empty($cnt))
 	        {
 	        	$res = array();//СКЛАДЫВАЕМ ТОП5 (5м индексом идет результат данного юзера)
@@ -39,6 +52,7 @@ class Userlottery extends AppModel {
 	        	}
 	        }
     	}
+//pr($res);
     	return $res;
     }
 }
