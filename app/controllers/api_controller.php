1847c823af912c21323a0a0e3271b6992ec74c6d
snow <snow@90f47898-201c-42e4-a952-0c166423ae87>
2011-07-26 09:59:23 +0000

    api initial _1
    git-svn-id: svn://svn.anka.ws/videoxq@370 90f47898-201c-42e4-a952-0c166423ae87

--- app/controllers/api_controller.php
+++ app/controllers/api_controller.php
@@ -6,49 +6,86 @@
  * @author snowing
  */
 class ApiController extends Controller {
-    
+
     var $name = 'Api';
     var $layout = 'ajax';
     var $viewPath = 'api';
-    var $helpers = array('Html', 'javascript');
+    var $helpers = array('Html', 'javascript', 'XML');
     var $components = array();
     var $uses = array('Direction', 'News');
 
-    
-    
+    function BeforeFilter() {
+        parent::BeforeFilter();
+        $this->autoRender = false;
+    }
+
     function Login() {
-        
+        //$user->login();
     }
 
     function Logout() {
-        
+        //$user->logout();
     }
 
-    function GetCategories($cat_id=null) {
+    function GetMenu($cat_id=null) {
         if ($cat_id == null)
             $cat_id = 0;
-        
-        
-    }
-    
-    function GetItem() {
-        $item_type = filter_var($_GET['item_type'],FILTER_SANITIZE_STRING);
-        $item_id = filter_var($_GET['item_id'],FILTER_VALIDATE_INT);       
-        switch ($item_type){
-            case 'video':{
-                
-            }
-            default:{
-                
-            }
+        switch ($cat_id) {
+        case 0:{
+            $res = GetStartMenu();            
+            break;
         }
+        case 'video': {
+            //$res = GetVideoCat();
+            break;
+        }
+        case ' videocat': {
+            //$res=GetVideoCatItems();
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    if ($res)
+    echo $this->Xml->serialize($res);
+}
+
+
+function GetItem() {
+    $item_type = filter_var($_GET['item_type'], FILTER_SANITIZE_STRING);
+    $item_id = filter_var($_GET['item_id'], FILTER_VALIDATE_INT);
+    switch ($item_type) {
+        case 'video': {
+                $result = GetItemVideo($item_id);
+            }
+        case 'profile': {
+                //$res=GetProfileInfo();
+            }
+        default: {
+                
+            }
+    }
+    }
+
+    private function GetStartMenu(){
+        $data=array();
+        $data['vxq'][0]['name']='Video';
+        $data['vxq'][0]['type']='menu';
+        
+        $data['vxq'][1]['name']='Profile';
+        $data['vxq'][1]['type']='Item';
+        
+        $data['vxq'][2]['name']='Logout';
+        $data['vxq'][2]['type']='Action';
+        
         
     }
     
-    private function GetItemVideo($item_id){
+    
+    private  function GetItemVideo($item_id) {
         
     }
-    
 
 }
 
