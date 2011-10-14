99273e71a422dbdadc49ff55db0ccb47f440938f
ivan <ivan@90f47898-201c-42e4-a952-0c166423ae87>
2011-10-14 06:51:48 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@560 90f47898-201c-42e4-a952-0c166423ae87

--- app/controllers/directions_controller.php
+++ app/controllers/directions_controller.php
@@ -23,7 +23,7 @@ class DirectionsController extends AppController {
     function admin_up($id = null) {
         if (!empty($id) && $id) {
             $info = $this->Direction->moveup($id,1);
-            $this->Session->setFlash('Категория перемещена вверх.', true);
+            $this->Session->setFlash('Категория перемещена вверх!', true);
         }
         else{
             $this->Session->setFlash('Ошибка. Куда делcя id?', true);
@@ -36,7 +36,7 @@ class DirectionsController extends AppController {
     function admin_down($id = null) {
         if (!empty($id) && $id) {
             $info = $this->Direction->movedown($id,1);
-            $this->Session->setFlash('Категория перемещена вниз.', true);
+            $this->Session->setFlash('Категория перемещена вниз!', true);
         }
         else{
             $this->Session->setFlash('Ошибка. Куда делcя id?', true);
@@ -46,77 +46,144 @@ class DirectionsController extends AppController {
 
 //------------------------------------------------------------------------------
 
-function admin_add($id = null) {
-    $data = array();
-    if (!empty($this->data))
-    {
-        $validate = true;
-        //проверим входные данные для записи
-        $parent_id = intval($this->data['Direction']['parent_id']);
-        $title = $this->data['Direction']['title'];
-        $caption = $this->data['Direction']['caption'];
-        $txt = $this->data['Direction']['txt'];
-        $hidden = $this->data['Direction']['hidden'];
-        if (!$title) $validate = false;
+    function admin_add($id = null) {
+        $data = array();
+        if (!empty($this->data))
+        {
+            $validate = true;
+            //проверим входные данные для записи
+            //особо проверять не будем - админы ведь рулят изнутри,
+            //а не вредители :) + внутренняя проверка кейка есть воде бы.
+            $parent_id = intval($this->data['Direction']['parent_id']);
+            $title = $this->data['Direction']['title'];
+            $caption = $this->data['Direction']['caption'];
+            $txt = $this->data['Direction']['txt'];
+            $hidden = $this->data['Direction']['hidden'];
+            if (!$title) $validate = false;
 
-        if ($validate){
-            //создадим новый массив, а то мало ли что нам там прилетело по post
-            $new_data = array('Direction' => array(
-                'parent_id' => ($parent_id)? $parent_id : null,
-                'title' => $title,
-                'caption' => $caption,
-                'txt' => $txt,
-                'hidden' => $hidden
-            ));
+            if ($validate){
+                //создадим новый массив, а то мало ли что нам там прилетело по post
+                $new_data = array('Direction' => array(
+                    'parent_id' => ($parent_id)? $parent_id : null,
+                    'title' => $title,
+                    'caption' => $caption,
+                    'txt' => $txt,
+                    'hidden' => $hidden
+                ));
 
-        //пишем в БД!
-        if ($this->Direction->save($new_data)) {
-            $this->Session->setFlash('Категория добавлена', true);
-            $this->redirect(array('action'=>'index'));
-	        }
+            //пишем в БД!
+            if ($this->Direction->save($new_data)) {
+                $this->Session->setFlash('Категория добавлена!', true);
+                $this->redirect(array('action'=>'index'));
+                    }
+            }
+            else{
+                    $this->Session->setFlash('Ошибка. Заполните поля правильно', true);
+
+                    $directions_list = $this->Direction->generatetreelist(null, null, null, '');
+                    $data['directions_list'] = $directions_list;
+                    $this->set('data',$data);
+                    $this->redirect(array('action'=>'add'));
+            }
+
+        }else{
+            if (!empty($id) && $id){
+                $data['parent_id'] = $id;
+            }
+
+            $directions_list = $this->Direction->generatetreelist(null, null, null, '');
+            $data['directions_list'] = $directions_list;
+            $this->set('data',$data);
         }
-        else{
-                $this->Session->setFlash('Ошибка. Заполните поля правильно', true);
-
-                $directions_list = $this->Direction->generatetreelist(null, null, null, '');
-                $data['directions_list'] = $directions_list;
-                $this->set('data',$data);
-                $this->redirect(array('action'=>'add'));
-        }
-
-    }else{
-        if (!empty($id) && $id){
-            $data['parent_id'] = $id;
-        }
-
-        $directions_list = $this->Direction->generatetreelist(null, null, null, '');
-        $data['directions_list'] = $directions_list;
-        $this->set('data',$data);
     }
-}
 
 //------------------------------------------------------------------------------
 
     function admin_edit($id = null) {
-        if ( empty($id) || !$id) {
-            $this->Session->setFlash('Ошибка. Куда делcя id?', true);
-            $this->redirect(array('action'=>'index'));
-        }
-    	if (!empty($this->data))
-    	{
-	    	$this->data['Direction']['modified'] = date('Y-m-d H:i:s');
-//	        if ($this->Direction->save($this->data)) {
+        $data = array();
+        if (!empty($this->data))
+        {
 
-	            $this->Session->setFlash('Категория изменена', true);
-	            $this->redirect(array('action'=>'index'));
-//	        }
-    	}
+            $validate = true;
+            //проверим входные данные для записи
+            //особо проверять не будем - админы ведь рулят изнутри,
+            //а не вредители :) + внутренняя проверка кейка есть воде бы.
+            $id = intval($this->data['Direction']['id']);
+            $parent_id = intval($this->data['Direction']['parent_id']);
+            $title = $this->data['Direction']['title'];
+            $caption = $this->data['Direction']['caption'];
+            $txt = $this->data['Direction']['txt'];
+            $hidden = $this->data['Direction']['hidden'];
+            if(!$id){
+                $this->Session->setFlash('Ошибка. Куда делcя id?', true);
+                $this->redirect(array('action'=>'index'));
+            }
+            if (!$title) $validate = false;
 
-        if (!empty($id)) {
-            $info = $this->Direction->read(null, $id);
-            $this->set('info', $info);
+            if ($validate){
+                //создадим новый массив, а то мало ли что нам там прилетело по post
+                $new_data = array('Direction' => array(
+                    'id' => $id,
+                    'parent_id' => ($parent_id)? $parent_id : null,
+                    'title' => $title,
+                    'caption' => $caption,
+                    'txt' => $txt,
+                    'hidden' => $hidden
+                ));
+                //проверим сменили ли parent_id, если не сменили то уберем его
+                //из массива, да бы не напрягать высоконагруженный сервер :)))
+                //бесполезной переиндексацией дерева (так советуют в манулае
+                //кейка :))
+                if ($this->data['Direction']['parent_id'] == $this->data['Direction']['old_parent_id']){
+                    unset($data['parent_id']);
+                }
+
+            //пишем в БД!
+            if ($this->Direction->save($new_data)) {
+                $this->Session->setFlash('Категория изменена!', true);
+                $this->redirect(array('action'=>'index'));
+                    }
+               else{
+                    $this->Session->setFlash('Произошла ошибка', true);
+                    $data = $this->data['Direction'];
+                    $directions_list = $this->Direction->generatetreelist(null, null, null, '');
+                    $data['directions_list'] = $directions_list;
+                    $data['old_parent_id'] = $data['parent_id'];
+                    $this->set('data',$data);
+                    //$this->redirect(array('action'=>'index'));
+               }
+            }
+            else{
+                    $this->Session->setFlash('Ошибка. Заполните поля правильно', true);
+                    $data = $this->data['Direction'];
+                    $directions_list = $this->Direction->generatetreelist(null, null, null, '');
+                    $data['directions_list'] = $directions_list;
+                    $data['old_parent_id'] = $data['parent_id'];
+                    $this->set('data',$data);
+//                    $this->redirect(array('action'=>'edit'));
+            }
+
+        }else{
+            if (!empty($id) && $id){
+
+                //вытащим данные для редактирования
+                $result = $this->Direction->find('first', array(
+                        'conditions' => array('id' => $id),
+                        'fields' => array()
+                    ));
+                $data = $result['Direction'];
+                //заполним вспомогательные поля
+                $directions_list = $this->Direction->generatetreelist(null, null, null, '');
+                $data['directions_list'] = $directions_list;
+                $data['old_parent_id'] = $data['parent_id'];
+                //вперед и с песней!
+                $this->set('data',$data);
+            }
+            else{
+                $this->Session->setFlash('Ошибка. Куда делcя id?', true);
+                $this->redirect(array('action'=>'index'));
+            }
         }
-        $this->set('authUser', $this->authUser);
     }
 
 //------------------------------------------------------------------------------
@@ -153,6 +220,7 @@ function admin_add($id = null) {
 //------------------------------------------------------------------------------
 
     function admin_reorder(){
+        //$result = $this->Direction->reorder();
         $this->Session->setFlash('Переиндексация сруктуры категорий прошло успешно!', true);
 	$this->redirect(array('action'=>'index'));
     }
@@ -160,6 +228,7 @@ function admin_add($id = null) {
 //------------------------------------------------------------------------------
 
     function admin_recover(){
+        //$result = $this->Direction->recover();
         $this->Session->setFlash('Восстановление сруктуры категорий прошло успешно!', true);
 	$this->redirect(array('action'=>'index'));
     }
@@ -167,9 +236,19 @@ function admin_add($id = null) {
 //------------------------------------------------------------------------------
 
     function admin_verify(){
-        $this->Direction->verify();
-        $this->Session->setFlash('Проверка сруктуры категорий прошло успешно!', true);
-	$this->redirect(array('action'=>'index'));
+        $result = $this->Direction->verify();
+        if ($result && !is_array($result)){
+            $this->Session->setFlash('Проверка сруктуры категорий прошло успешно!', true);
+            $this->redirect(array('action'=>'index'));
+        }
+        else{
+            $data = array(
+                'message' => 'Ошибки в структуре категорий:',
+                'result' => $result
+            );
+            $this->set('data',$data);
+        }
+
     }
 //------------------------------------------------------------------------------
 
