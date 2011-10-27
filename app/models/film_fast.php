21751d0d9ebbc47c7e75cb8d3658cfb63f6723cf
snow <snow@90f47898-201c-42e4-a952-0c166423ae87>
2011-10-27 08:16:12 +0000

    git-svn-id: svn://svn.anka.ws/videoxq@787 90f47898-201c-42e4-a952-0c166423ae87

--- app/models/film_fast.php
+++ app/models/film_fast.php
@@ -354,8 +354,8 @@ class FilmFast extends AppModel {
             $pagination['Film']['conditions'] = array('is_license' => '1', 'active' => 1);
             $pagination['Film']['page'] = $page;
             $pagination['Film']['limit'] = $per_page;
-            $pagination['Film']['order'] = 'Film.year';
-            $pagination['Film']['direction']='desc';
+            //$pagination['Film']['order'] = 'Film.year';
+            //$pagination['Film']['direction']='desc';
             $pagination['Film']['sphinx']['matchMode'] = SPH_MATCH_ALL;
             $pagination['Film']['sphinx']['index'] = array('videoxq_films_lic'); //ИЩЕМ ПО ИНДЕКСУ ФИЛЬМОВ
             $pagination['Film']['sphinx']['sortMode'] = array(SPH_SORT_EXTENDED => '@relevance DESC');
