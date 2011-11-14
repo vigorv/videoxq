98e5dcdf5972fae03f90642317986613f4386c18
haidiamondcru <hai@diamondc.ru>
2011-11-14 14:01:18 +0600

    админка MetaTags add/delete/index + views (промежуточный вариант для теста)
    Signed-off-by: haidiamondcru <hai@diamondc.ru>

--- app/controllers/meta_tags_controller.php
+++ app/controllers/meta_tags_controller.php
@@ -6,12 +6,13 @@ class metaTagsController extends AppController {
     var $uses = array('MetaTag');
 
     var $helpers = array('Html','Form','Javascript','Autocomplete');
-    /*    
-    var $components = array('Phonetics', 'ExcelImport' => array(
-                                                'param1' => '111111',
-                                                'param2' => '222222'
-                                             ));
 
+     /*
+     * управление мета-тегами
+     * 
+     * 
+     * 
+     */
     function index(){
         $data = $this->MetaTag->find('all');
         $this->set('data', $data);
@@ -24,8 +25,6 @@ class metaTagsController extends AppController {
      * 
      */
     function admin_index(){
-        
-
         //инициализируем массив данных для вьюхи
         $metatags = array();
         //url пустой. т.к. выбираем записи метатегов для всех url'ов, и в этом
@@ -35,8 +34,91 @@ class metaTagsController extends AppController {
         $metatags['count'] = $this->MetaTag->getMetaTagCountByURL($url,true);
         $metatags['data'] = $this->MetaTag->getMetaTagByURL($url,true);
         $this->set('metatags',$metatags);
-    }     
         
+        $res = $this->_url_clear('/');
+    }     
+    
+    function admin_add(){
+        //инициализируем массив данных для вьюхи
+        $data = array();
+        if (!empty($this->data))
+        {
+            $validate = true;
+            //проверим входные данные для записи
+            //особо проверять не будем - админы ведь рулят изнутри,
+            //а не вредители :) да и пустым может быть любое поле!
+            $url = filter_var($this->data['MetaTags']['url'], FILTER_SANITIZE_STRING);
+            $title = filter_var($this->data['MetaTags']['title'], FILTER_SANITIZE_STRING);
+            $description = filter_var($this->data['MetaTags']['description'], FILTER_SANITIZE_STRING);
+            $keywords = filter_var($this->data['MetaTags']['keywords'], FILTER_SANITIZE_STRING);
+            $title_en = filter_var($this->data['MetaTags']['title_en'], FILTER_SANITIZE_STRING);
+            $description_en = filter_var($this->data['MetaTags']['description_en'], FILTER_SANITIZE_STRING);
+            $keywords_en = filter_var($this->data['MetaTags']['keywords_en'], FILTER_SANITIZE_STRING);            
+            $order = intval($this->data['MetaTags']['order']);
+            $isbase = intval($this->data['MetaTags']['isbase']);
+
+            if ($validate){
+                //создадим новый массив, а то мало ли что нам там прилетело по post
+                $new_data = array('Direction' => array(
+                    'url' => $url,
+                    'title' => $title,
+                    'description' => $description,
+                    'keywords' => $keywords,
+                    'title_en' => $title_en,
+                    'description_en' => $description_en,
+                    'keywords_en' => $keywords_en,
+                    'order' => $order,
+                    'isbase' => $isbase
+                ));
+
+            //пишем в БД!
+            if ($this->MetaTag->save($new_data)) {
+                $this->Session->setFlash('Запись о мета-тегах добавлена!', true);
+                $this->redirect(array('action'=>'index'));
+                    }
+            }
+            else{
+                    $this->Session->setFlash('Ошибка. Заполните поля правильно', true);
+                    //вернем юзеру его набранные строчки, 
+                    //пусть делает работу над ошибками )))))
+                    $data = $this->data;
+                    $this->set('data',$data);
+                    $this->redirect(array('action'=>'add'));
+            }
+        }else{        
+            $this->set('data',$data);
+        }
+        
+    }
+    
+    function admin_edit($id=0){
+        //инициализируем массив данных для вьюхи
+        $metatags = array();
+        //url пустой. т.к. выбираем записи метатегов для всех url'ов, и в этом
+        //нам поможет 2й параметр вызова ф-ции модели = true
+        $url = '';
+        //общее количество записаей метатегов
+        $metatags['count'] = $this->MetaTag->getMetaTagCountByURL($url,true);
+        $metatags['data'] = $this->MetaTag->getMetaTagByURL($url,true);
+        $this->set('metatags',$metatags);
+        
+        $res = $this->_url_clear('/');
+    }
+        
+    function admin_delete($id=0){
+        //инициализируем массив данных для вьюхи
+        $metatags = array();
+        //url пустой. т.к. выбираем записи метатегов для всех url'ов, и в этом
+        //нам поможет 2й параметр вызова ф-ции модели = true
+        $url = '';
+        //общее количество записаей метатегов
+        $metatags['count'] = $this->MetaTag->getMetaTagCountByURL($url,true);
+        $metatags['data'] = $this->MetaTag->getMetaTagByURL($url,true);
+        $this->set('metatags',$metatags);
+        
+        $res = $this->_url_clear('/');
+    }
+            
 //------------------------------------------------------------------------------
     /*очистка url от начальных символов "/", "http://", "www" 
      * и пробелов по краям
@@ -45,6 +127,7 @@ class metaTagsController extends AppController {
      * @return string $url
      */
     function _url_clear($url=''){        
+/*        
         $url = str_replace('http://www.', '', trim($url));
         $url = str_replace('http://', '', $url);
         $url = str_replace($_SERVER['SERVER_NAME'], '', $url);
@@ -53,6 +136,8 @@ class metaTagsController extends AppController {
             $url = substr($url, 1, strlen($url)-1);
         }
         return $url;
+ */
+        return $this->Metatags->fixUrl($url);
     }
 
 }
---
+++ app/views/meta_tags/admin_edit.ctp
@@ -0,0 +1,37 @@
+<?php
+$html->css('adm_meta_tags','',array(),false);
+$javascript->link('adm_meta_tags.js', false);
+?>
+<script type="text/javascript">
+jQuery(document).ready(function() {
+});
+</script>
+<div class="form">
+<?php
+	echo $form->create('Direction', array('action' => 'edit', 'enctype' => 'multipart/form-data'));
+?>
+    <fieldset>
+         <legend>Добавление записи о мета-тегах</legend>
+<?php
+        echo $form->input('url', array('label' => 'url', 'value' => (!empty($data['url']) ? $data['url'] : '')));
+        echo $form->input('title', array('label' => 'title', 'value' => (!empty($data['title']) ? $data['title'] : '')));
+        echo $form->input('description', array('label' => 'description', 'value' => (!empty($data['description']) ? $data['description'] : '')));
+        echo $form->input('keywords', array('label' => 'keywords', 'value' => (!empty($data['keywords']) ? $data['keywords'] : '')));
+        echo $form->input('title_en', array('label' => 'title_en', 'value' => (!empty($data['title_en']) ? $data['title_en'] : '')));
+        echo $form->input('description_en', array('label' => 'description_en', 'value' => (!empty($data['description_en']) ? $data['description_en'] : '')));
+        echo $form->input('keywords_en', array('label' => 'keywords_en', 'value' => (!empty($data['keywords_en']) ? $data['keywords_en'] : '')));
+        echo $form->input('order', array('label' => 'order', 'value' => (!empty($data['order']) ? $data['order'] : '')));
+        echo $form->input('isbase', array('label' => 'isbase', 'value' => 1, 'checked' => (!empty($data['isbase']) ? 'checked' : '')));
+	echo $form->submit('Применить');
+?>
+    </fieldset>
+    <?php echo $form->end();?>
+</div>
+<?php echo $html->link('Вернуться к списку мета-тегов', array('action'=>'index'),array('class'=>'a_btn','style'=>'display: block; clear: both'));?>
+<script type="text/javascript">
+
+      $(document).ready(function() {
+
+      });
+
+</script>
