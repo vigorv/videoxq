ad29832897310f638d8478826782498ba1f48a77
hai <hai@diamondc.ru>
2011-12-15 09:59:58 +0600

    test

--- app/models/meta_tag.php
+++ app/models/meta_tag.php
@@ -178,5 +178,10 @@ class MetaTag extends AppModel {
         }
         return $metatags;
     }
+    public function getMetaTagsByURLMask2($url='', $page=1, $perpage=0){
+	$metatags = array();
+
+        return $metatags;
+    }    
 }
 ?>
