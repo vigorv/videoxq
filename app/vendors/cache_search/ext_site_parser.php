ed63c527750ad92fbd6500df5d0aa7b15cf4c776
hai <hai@diamondc.ru>
2012-01-17 11:56:26 +0600

    контрольная точка

--- app/vendors/cache_search/ext_site_parser.php
+++ app/vendors/cache_search/ext_site_parser.php
@@ -57,6 +57,10 @@ class extSiteParser
 			case "animebar":
 				$data = $this->parseAnimebarRow($row);
 			break;
+                    
+			case "videoxq":
+				$data = $this->parseVideoxqRow($row);
+			break;                    
 		}
 		if (!empty($data))
 		{
@@ -66,7 +70,7 @@ class extSiteParser
 			$data['actors'] = mb_substr($data['actors'], 0, 254, 'utf-8');
 			$data['directors'] = mb_substr($data['directors'], 0, 254, 'utf-8');
 			$data['poster'] = mb_substr($data['poster'], 0, 254, 'utf-8');
-			$data['url'] = mb_substr($data['poster'], 0, 254, 'utf-8');
+			$data['url'] = mb_substr($data['url'], 0, 254, 'utf-8');
 		}
 		return $data;
 	}
@@ -179,4 +183,195 @@ class extSiteParser
 		$data['url'] = 'http://animebar.ru/' . $row['alt_name'] . '.html';
 		return $data;
 	}
+        
+        
+	/**
+	 * разбор записи с сайта videoxq
+	 *
+	 * @param mixed $row
+	 * @return mixed
+	 */
+	public function parseVideoxqRow($row)
+	{
+
+		$data = array();
+
+		$matches = array();
+		preg_match('/src="([^"]{1,})"/', $row['short_story'], $matches);
+		$data['poster'] = '';
+		if (!empty($matches[0]))
+		{
+        	$matches[0] = substr($matches[0],5);
+			$data['poster'] = iconv('windows-1251', 'utf-8//IGNORE', $matches[0]);
+		}
+        //iconv сделать
+		$data['id_original'] = $row['id'];
+		$data['title'] = iconv('windows-1251', 'utf-8//IGNORE', $row['title']);
+		$data['title_original'] = iconv('windows-1251', 'utf-8//IGNORE', $row['title2']);
+		$data['created_original'] = $row['date'];
+		$data['modified_original'] = $row['date'];
+		$data['hidden'] = $row['approve'];
+		$data['url'] = 'http://rumedia.ws/' . $row['alt_name'] . '.html';
+                $data['year']			= 0;
+		$data['country']		= '';
+		$data['directors']		= '';
+		$data['actors']			= '';
+		$xfields = explode('||', $row['xfields']);
+		foreach ($xfields as $xfield)
+		{
+			$xf = explode('|', $xfield);
+			switch ($xf[0])
+			{
+				case "m_year":
+				case "games_year":
+				case "soft_year":
+					$data['year'] = intval($xf[1]);
+				break;
+				case "m_country":
+				case "games_country":
+				case "soft_country":
+					$data['country'] = iconv('windows-1251', 'utf-8//IGNORE', $xf[1]);
+				break;
+				case "m_director":
+					$data['directors'] = iconv('windows-1251', 'utf-8//IGNORE', $xf[1]);
+				break;
+				case "m_actors":
+					$data['actors'] = iconv('windows-1251', 'utf-8//IGNORE', $xf[1]);
+				break;
+			}
+		}
+		return $data;
+	}
+        
 }
+
+
+/*
+ 
+SELECT
+`Film`.`id` AS `id_original`,
+`Film`.`created` AS `created_original`,
+`Film`.`modified` AS `modified_original`, 
+(`Film`.`active` != 1) AS `hidden`,
+`Film`.`title` AS `title`,
+`Film`.`title_en` AS `title_original`,
+`Film`.`year` AS `year`,
+GROUP_CONCAT(DISTINCT `Country`.`title` SEPARATOR ", ") AS `country`,
+GROUP_CONCAT(DISTINCT DirPerson.name SEPARATOR ", ") AS `directors`,
+1 AS `actors`,
+GROUP_CONCAT(DISTINCT Genres.title SEPARATOR ", ") AS `genres`,
+3 AS `site_id`,
+0 AS `poster`,
+0 AS `url`,
+`Film`.`is_license` AS `is_license`
+
+
+FROM 
+`films` AS `Film` 
+
+LEFT JOIN 
+`films_genres` AS `FilmsGenres` 
+ON 
+(`Film`.`id` = `FilmsGenres`.`film_id`) 
+LEFT JOIN 
+`genres` AS `Genres` 
+ON 
+(`FilmsGenres`.`genre_id` = `Genres`.`id`) 
+ 
+LEFT JOIN 
+`countries_films` AS `FilmsCountries` 
+ON 
+(`Film`.`id` = `FilmsCountries`.`film_id`) 
+LEFT JOIN 
+`countries` AS `Country` 
+    ON 
+(`FilmsCountries`.`country_id` = `Country`.`id`)
+
+
+LEFT JOIN 
+`films_persons` AS `FilmsPersons` 
+ON 
+(`Film`.`id` = `FilmsPersons`.`film_id`) 
+LEFT JOIN 
+`persons` AS `DirPerson` 
+ON 
+(`FilmsPersons`.`person_id` = `DirPerson`.`id`)
+
+LEFT JOIN 
+`persons_professions` AS `DirPersonsProfessions` 
+ON 
+(`DirPersonsProfessions`.`person_id` = `DirPerson`.`id`  AND
+ `DirPersonsProfessions`.`profession_id` = 1
+) 
+
+WHERE
+`DirPersonsProfessions`.`profession_id` = 1  
+
+
+GROUP BY `Film`.`id`
+
+
+ */
+
+/*
+(SELECT distinct id_cost
+  FROM Message108 where id_directory=39)
+
+ */
+/*
+SELECT
+`Film`.`id` AS `id_original`,
+`Film`.`created` AS `created_original`,
+`Film`.`modified` AS `modified_original`, 
+(`Film`.`active` != 1) AS `hidden`,
+`Film`.`title` AS `title`,
+`Film`.`title_en` AS `title_original`,
+`Film`.`year` AS `year`,
+GROUP_CONCAT(DISTINCT `Country`.`title` SEPARATOR ", ") AS `country`,
+GROUP_CONCAT(DISTINCT DirPerson.name SEPARATOR ", ") AS `directors`,
+1 AS `actors`,
+GROUP_CONCAT(DISTINCT Genres.title SEPARATOR ", ") AS `genres`,
+3 AS `site_id`,
+0 AS `poster`,
+0 AS `url`,
+`Film`.`is_license` AS `is_license`
+
+
+FROM 
+`films` AS `Film` 
+
+LEFT JOIN 
+`films_genres` AS `FilmsGenres` 
+ON 
+(`Film`.`id` = `FilmsGenres`.`film_id`) 
+LEFT JOIN 
+`genres` AS `Genres` 
+ON 
+(`FilmsGenres`.`genre_id` = `Genres`.`id`) 
+ 
+LEFT JOIN 
+`countries_films` AS `FilmsCountries` 
+ON 
+(`Film`.`id` = `FilmsCountries`.`film_id`) 
+LEFT JOIN 
+`countries` AS `Country` 
+    ON 
+(`FilmsCountries`.`country_id` = `Country`.`id`)
+
+
+LEFT JOIN 
+`films_persons` AS `FilmsPersons` 
+ON 
+(`Film`.`id` = `FilmsPersons`.`film_id`) 
+LEFT JOIN 
+(
+SELECT * FROM persons
+)
+ AS `DirPerson` 
+ON 
+(`FilmsPersons`.`person_id` = `DirPerson`.`id`)
+
+
+GROUP BY `Film`.`id`
+ */
+
